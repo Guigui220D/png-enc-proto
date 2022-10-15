0f4a017a8118481620d55533721660b6e7353cca
Guillaume DEREX <gderex8@orange.fr>
2022-10-15 15:15:15 +0200

    Fixed deflate misuse (flush instead of close)

--- zlib_compressor.zig
+++ zlib_compressor.zig
@@ -47,7 +47,7 @@ pub fn ZlibCompressor(comptime WriterType: type) type {
 
         /// Ends a zlib block with the checksum
         pub fn end(self: *Self) !void {
-            try self.compressor.flush();
+            try self.compressor.close();
             self.compressor.deinit();
             // Write the checksum
             var wr = self.raw_writer;
